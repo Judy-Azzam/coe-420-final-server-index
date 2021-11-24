c28aa27761cf20651a508d18c5891d1d3fb6a1f4
Judy-Azzam <78794695+Judy-Azzam@users.noreply.github.com>
2021-11-25 02:53:23 +0400

    posting blog added but not completed
    SignUp and login for investors table added signup and login for startups added

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const db = mysql.createConnection({
     password: "password",
     database: "suitsup",
   });
-  app.post("/create", (req, res) => {
+  app.post("/createstartup", (req, res) => {
     const companyName = req.body.companyName;
     const firstName = req.body.firstName;
     const lastName = req.body.lastName;
@@ -19,7 +19,7 @@ const db = mysql.createConnection({
     const password =  req.body.password;
 
     db.query(
-      "INSERT INTO suitsup.users (companyName, firstName, lastName, email, password) VALUES (?,?,?,?,?)",
+      "INSERT INTO suitsup.startups (companyName, firstName, lastName, email, password) VALUES (?,?,?,?,?)",
       [companyName,firstName, lastName, email, password],
       (err, result) => {
         if (err) {
@@ -30,12 +30,31 @@ const db = mysql.createConnection({
       }
     );
   });
-  app.post("/login", (req, res) => {
+  app.post("/createinvestor", (req, res) => {
+    const companyName = req.body.companyName;
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
     const email =  req.body.email;
     const password =  req.body.password;
 
     db.query(
-      "SELECT * FROM suitsup.users WHERE email = ? AND password = ?",
+      "INSERT INTO suitsup.investors (companyName, firstName, lastName, email, password) VALUES (?,?,?,?,?)",
+      [companyName,firstName, lastName, email, password],
+      (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send("Values Inserted");
+        }
+      }
+    );
+  });
+  app.post("/startupslogin", (req, res) => {
+    const email =  req.body.email;
+    const password =  req.body.password;
+
+    db.query(
+      "SELECT * FROM suitsup.startups WHERE email = ? AND password = ?",
       [email, password],
       (err, result) => {
         if (err) {
@@ -50,6 +69,62 @@ const db = mysql.createConnection({
       
     );
   });
+  app.post("/investorslogin", (req, res) => {
+    const email =  req.body.email;
+    const password =  req.body.password;
+
+    db.query(
+      "SELECT * FROM suitsup.investors WHERE email = ? AND password = ?",
+      [email, password],
+      (err, result) => {
+        if (err) {
+          console.log({err: err});
+        } 
+        if(result.length > 0){
+          res.send(result);
+        }else{
+          res.send({message: "Wrong email/ password combination!"});
+        }
+      }
+      
+    );
+  });
+  app.get("/employees", (req, res) => {
+    db.query("SELECT * FROM suitsup.investors", (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    });
+});
+ 
+app.put("/update", (req, res) => {
+    const id = req.body.id;
+    const wage = req.body.wage;
+    db.query(
+      "UPDATE suitsup.investors SET wage = ? WHERE id = ?",
+      [wage, id],
+      (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send(result);
+        }
+      }
+    );
+});
+ 
+app.delete("/delete/:id", (req, res) => {
+    const id = req.params.id;
+    db.query("DELETE FROM suitsup.investors WHERE id = ?", id, (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    });
+});
 app.listen(3001, () => {
     console.log("Yey, your server is running on port 3001");
   });
